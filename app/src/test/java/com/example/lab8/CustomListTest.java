97f184434970e49f4e11149c37740efb454a09fe
mansooranis <mansooranis60@gmail.com>
2023-03-07 17:55:40 -0700

    RED: added testCountCities

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -45,4 +45,13 @@ public class CustomListTest {
         assertEquals(false, list.hasCity(city));
     }
 
+    @Test
+    public void testCountCities(){
+        CustomList list = new CustomList(null, cityList);
+        City city = new City("Edmonton", "AB");
+        City city2 = new City("Calgary", "AB");
+        list.addCity(city);
+        list.addCity(city2);
+        assertEquals(2, list.countCities());
+    }
 }
