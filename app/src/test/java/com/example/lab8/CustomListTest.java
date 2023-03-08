1b6cde7b15bbad2e0be86b451f6d969fc872537e
mansooranis <mansooranis60@gmail.com>
2023-03-07 17:41:10 -0700

    RED: TEST failed

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,19 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private ArrayList<City> cityList = new ArrayList<>();
+
+    @Before
+    public void init(){
+
+    }
+
+    @Test
+    public void testAddCity(){
+        CustomList list = new CustomList(null, cityList);
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(1,list.getCount());
+    }
 
 }
